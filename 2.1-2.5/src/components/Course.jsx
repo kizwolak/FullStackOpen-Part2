ea32129ffd7927c4f2bccf723aaadf8c83fdaabe
kizwolak <ki.zwolak@gmail.com>
2023-10-05 17:15:24 -0300

    Enable arbitrary course number

--- 2.1-2.5/src/components/Course.jsx
+++ 2.1-2.5/src/components/Course.jsx
@@ -1,22 +1,30 @@
 const Course = (props) => {
-  console.log(props.course);
-  const { id, name, parts } = props.course;
-  console.log(name);
-  const displayParts = (parts) => {
-    return parts.map((x) => (
-      <div key={x.id}>
-        {x.name} {x.exercises}
-      </div>
-    ));
+  const displayCourses = (courses) => {
+    const courseElements = [];
+    console.log(courses);
+    for (const course of courses) {
+      console.log(course);
+      const { id, name, parts } = course;
+      const displayParts = (parts) => {
+        return parts.map((x) => (
+          <div key={x.id}>
+            {x.name} {x.exercises}
+          </div>
+        ));
+      };
+      courseElements.push(
+        <div key={Math.random()}>
+          <h1>{name}</h1>
+          {displayParts(parts)}
+          <b>
+            total of {parts.reduce((acc, x) => acc + x.exercises, 0)} exercises
+          </b>
+        </div>
+      );
+    }
+    return courseElements;
   };
-
-  return (
-    <>
-      <h1>{name}</h1>
-      {displayParts(parts)}
-      <b>total of {parts.reduce((acc, x) => acc + x.exercises, 0)} exercises</b>
-    </>
-  );
+  return <>{displayCourses(props.courses)}</>;
 };
 
 export default Course;
