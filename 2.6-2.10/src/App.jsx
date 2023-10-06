bdda7199d325feb90c2529e711dc9883da13a958
kizwolak <ki.zwolak@gmail.com>
2023-10-06 00:37:07 -0300

    Add dummy data

--- 2.6-2.10/src/App.jsx
+++ 2.6-2.10/src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
@@ -20,7 +25,7 @@ const App = () => {
       alert(`${newName} has already been added to the phonebook.`);
       return;
     } else {
-      setPersons(persons.concat({ name: newName, phone: newNumber }));
+      setPersons(persons.concat({ name: newName, number: newNumber }));
       setNewName("");
       setNewNumber("");
     }
@@ -43,7 +48,7 @@ const App = () => {
       <h2>Numbers</h2>
       {persons.map((x) => (
         <p key={Math.random()}>
-          {x.name} {x.phone}
+          {x.name} {x.number}
         </p>
       ))}
     </div>
